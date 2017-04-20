bbbd7eb611a287907a81c572f1555adadc058708
Umutari <umutariy@gmail.com>
2017-04-20 14:50:40 +0000

    changes

--- layout/menu.php
+++ layout/menu.php
@@ -6,15 +6,15 @@
 <body>
 	<header id="header">
 		<div class="container">
-			<a href="<?php echo SERVER_BASE_URL; ?>index.php" id="logo" title="ASGARDIAN NUSERY SCHOOL">ASGARDIAN NUSERY SCHOOL</a>
+			<a href="<?php echo BASE_URL; ?>index.php" id="logo" title="ASGARDIAN NUSERY SCHOOL">ASGARDIAN NUSERY SCHOOL</a>
 			<div class="menu-trigger"></div>
 			<nav id="menu">
 				<ul>
-					<li><a href="<?php echo SERVER_BASE_URL; ?>pages/ourPrograms.php">Our Programs</a></li>
-					<li><a href="<?php echo SERVER_BASE_URL; ?>pages/admissionspage1.php">Admission</a></li>
+					<li><a href="<?php echo BASE_URL; ?>pages/ourPrograms.php">Our Programs</a></li>
+					<li><a href="<?php echo BASE_URL; ?>pages/admissionspage1.php">Admission</a></li>
 				</ul>
 				<ul>
-					<li><a href="<?php echo SERVER_BASE_URL; ?>pages/events.php">Events</a></li>
+					<li><a href="<?php echo BASE_URL; ?>pages/events.php">Events</a></li>
 					<li><a href="#fancy" class="get-contact">Contact</a></li>
 
 					<li><a href="#login" class="get-contact">Login</a></li>
