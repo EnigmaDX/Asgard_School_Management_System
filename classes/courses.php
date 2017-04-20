8a366d1716fd58fe37e270ba6d36b6c0745e6c90
nanaadjoabentil <adjoacash@hotmail.com>
2017-04-20 15:17:37 +0000

    actual file with course class

---
+++ classes/courses.php
@@ -0,0 +1,101 @@
+<?php
+
+//include database file
+include_once("../database/Connection.php");
+
+//include courseTeacher file
+include_once("courseTeacher.php");
+
+//class for courses
+class Courses extends Connection
+{
+  //properties
+  private $courseCode;
+  private $courseName;
+  private $courseTeacher;
+  private $courseVenue;
+  private $class;
+  private $grade;
+
+  //__constructor
+  //calling setter methods
+  $this->setCourseCode($courseCode);
+  $this->setCourseName($courseName);
+  $this->setCourseTeacher($courseTeacher);
+  $this->setCourseVenue($courseVenue);
+  $this->setClass($class);
+  $this->setGrade($grade);
+
+  //calling getter methods
+  $this->getCourseCode($courseCode);
+  $this->getCourseName($courseName);
+  $this->getCourseTeacher($courseTeacher);
+  $this->getCourseVenue($courseVenue);
+  $this->getClass($class);
+  $this->getGrade($grade);
+
+  //methods
+  //setter methods
+  public function setCourseCode($courseCode)
+  {
+    $this->courseCode = $courseCode;
+  }
+
+  public function setCourseName($courseName)
+  {
+    $this->courseName = $courseName;
+  }
+
+  public function setCourseTeacher($courseTeacher)
+  {
+    $this->courseTeacher = $courseTeacher->getStaffFname();
+  }
+
+  public function setCourseVenue($courseVenue)
+  {
+    $this->courseVenue = $courseVenue;
+  }
+
+  public function setClass($class)
+  {
+    $this->class = $class;
+  }
+
+  public function setGrade($grade)
+  {
+    $this->grade = $grade;
+  }
+
+  //getter methods
+  public function getCourseCode()
+  {
+    return $this->courseCode;
+  }
+
+  public function getCourseName()
+  {
+    return $this->courseName;
+  }
+
+  public function getCourseTeacher()
+  {
+    return $this->courseTeacher;
+  }
+
+  public function getCourseVenue()
+  {
+    return $this->courseVenue;
+  }
+
+  public function getClass()
+  {
+    return $this->class;
+  }
+
+  public function getGrade()
+  {
+    return $this->grade;
+  }
+}
+
+?>
