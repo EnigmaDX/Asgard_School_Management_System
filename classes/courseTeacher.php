ab4881ad93fc84fc70da2cddf1c17d7d9ee8404a
nanaadjoabentil <adjoacash@hotmail.com>
2017-04-20 15:31:29 +0000

    Updated courseTeacher.php

--- classes/courseTeacher.php
+++ classes/courseTeacher.php
@@ -1,4 +1,8 @@
 <?php
+/**
+*@author Nana Adjoa S. Bentil
+*@version 1.1
+**/
 
 //include database file
 include_once("../database/Connection.php");
@@ -9,8 +13,20 @@ class courseTeacher extends Connection
   private $staffID;
   private $staffFname;
   private $staffLname;
+  
+  // __ constructor
+  //calling setter methods
+  $this->setStaffID($staffID);
+  $this->setStaffFname($staffFname);
+  $this->setStaffLname($staffLname);
 
+  //calling getter methods
+  $this->getStaffID();
+  $this->getStaffFname();
+  $this->getStaffLname();
+  
   //methods
+  //setter methods
   public function setStaffID($staffID)
   {
     $this->staffID = $staffID;
@@ -25,7 +41,8 @@ class courseTeacher extends Connection
   {
     $this->staffLname = $staffLname;
   }
-
+  
+  //getter methods
   public function getStaffID()
   {
     return $this->$staffID;
